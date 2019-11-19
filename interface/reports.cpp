8d6f6699d720aa28d66f463973a8359d9d6bb3af
vitorassis <vitor.a.camargo@live.com>
2019-11-19 18:46:00 -0300

    new file:   interface/reports.cpp 	new file:   relatorios/geral.txt 	new file:   relatorios/jogadores/vitor.txt

---
+++ interface/reports.cpp
@@ -0,0 +1,38 @@
+void showReportScreen(){
+//	bubbleSortSubscriptionByGamerName();
+	selectionSortSubscritpionsByChampionshipCode();
+	char fileName[40];
+	strcpy(fileName, "relatorios\\geral.txt");
+	FILE *report = fopen(fileName, "w");
+	int subAddr = 0;
+	int champCode = 0;
+	_championship champ;
+	_gamer gamer;
+	_game game;
+	
+	_subscription sub = getSubscription(0, subAddr);
+	while(sub.championship != 0){
+		champ = getChampionship(0, exaustiveSearchChampionshipByCode(sub.championship));
+		gamer = getGamer(0, indexedSearchGamerByCode(sub.gamer));
+		game = getGame(0, binarySearchGameByCode(champ.game)*sizeof(_game));
+		if(champCode != sub.championship){
+			if(champCode != 0)
+				fprintf(report, "\n-------------------------------------------------------------------------------------------------------------------\n");
+			
+			champCode = sub.championship;
+			
+			fprintf(report, "%s", champ.name);
+			fprintf(report, "\nResponsável: %s - %d - %s", champ.organizer, champ.year, game.name);
+			fprintf(report, "\nDescrição:\n");
+			fprintf(report, "%s", champ.description);
+			fprintf(report, "\n\nParticipantes (em ordem alfabética):\n");
+		}
+		fprintf(report, "-%s\n", gamer.name);
+		
+		subAddr += sizeof(sub);
+		sub = getSubscription(0, subAddr);
+	}
+	
+	closeFile(report);
+	system(fileName);
+}
